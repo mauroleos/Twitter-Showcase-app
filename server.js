b57f64e109be43327dc4e5f53029adb72a90606d
mauro Leos <mauroleos94@gmail.com>
2020-09-12 01:19:05 -0600

    Created server JS file

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("It's working!");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// const express = require("express");
+// const app = express();
+// const port = 3002;
+// //maybe 3002?
+
+// app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
