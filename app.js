df606ed0eafff81cb3ab7b750305653a4ea28704
mauro Leos <mauroleos94@gmail.com>
2020-09-22 00:14:51 -0500

    Worked on server

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 const path = require("path");
-const port = 3000;
+let port = process.env.PORT || 3000;
+
+// const access_token = "AAAAAAAAAAAAAAAAAAAAAJBgHgEAAAAAiyoNTAGGwbth1jMT05cOnJXFv6Y%3DGGW96OfGERoOvSQ1TyRIam79asCJQPC8352nuayWhwWL5bOHUE"
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
@@ -10,7 +13,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/statuses", function (req, res) {
-  res.send(statuses);
+  const fakeTweets = [
+    { text: "some tweet:", username: "elonmusk" },
+    { text: "another tweet:", username: "elonmusk" },
+  ];
+  res.send(fakeTweets);
 });
+
+fetch("/api/statuses")
+  .then((resp) => resp.json()) // Transform the data into json
+  .then(function (data) {
+    console.log(data);
+  });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
