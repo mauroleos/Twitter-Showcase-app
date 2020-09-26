4b8a3946e3d98397ed6ececeac802206880ac3c5
mauro Leos <mauroleos94@gmail.com>
2020-09-25 22:04:24 -0500

    Created functions to get tweets and users from Twitter

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path = require("path");
 const axios = require("axios");
 let port = process.env.PORT || 3000;
 
-// const access_token = "AAAAAAAAAAAAAAAAAAAAAJBgHgEAAAAAiyoNTAGGwbth1jMT05cOnJXFv6Y%3DGGW96OfGERoOvSQ1TyRIam79asCJQPC8352nuayWhwWL5bOHUE"
+const access_token =
+  "AAAAAAAAAAAAAAAAAAAAAJBgHgEAAAAAiyoNTAGGwbth1jMT05cOnJXFv6Y%3DGGW96OfGERoOvSQ1TyRIam79asCJQPC8352nuayWhwWL5bOHUE";
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
@@ -15,18 +16,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/statuses", function (req, res) {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
   axios
-    .get("https://api.twitter.com/1.1/search/tweets.json")
+    .get("https://api.twitter.com/1.1/search/tweets.json?q=nasa", config)
+    .then((response) => res.send(response.data))
+    .catch((error) => res.sendStatus(500));
+});
 
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
-  res.send(" ");
+app.get("/api/statuses/users", function (req, res) {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  };
+  axios
+    .get(
+      "https://api.twitter.com/1.1/users/show.json?screen_name=elonmusk",
+      config
+    )
+    .then((response) => res.send(response.data))
+    .catch((error) => res.sendStatus(500));
 });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
