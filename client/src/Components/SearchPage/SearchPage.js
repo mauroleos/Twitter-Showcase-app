cc8d9674063d9c4124c24a48d55112c6504a6f28
mauro Leos <mauroleos94@gmail.com>
2020-09-29 14:36:02 -0500

    Created new hook to update the value of tweets

--- client/src/Components/SearchPage/SearchPage.js
+++ client/src/Components/SearchPage/SearchPage.js
@@ -7,19 +7,18 @@ import { Card, Image } from "react-bootstrap";
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
-  this.state = {
-    tweet: {},
-  };
+  const [tweets, setTweets] = useState({});
 
   function searchTweets(event) {
     event.preventDefault();
     fetch(`/api/statuses?search_term=${searchTerm}`)
       .then((resp) => resp.json()) // Transform the data into json
       .then((data) => {
-        this.setState({
-          tweet: data,
+        setTweets({
+          tweets: data,
         });
       });
+    console.log(tweets);
   }
 
   function searchUsers(event) {
@@ -30,58 +29,45 @@ const SearchPage = () => {
   }
 
   const handleChange = (e) => setSearchTerm(e.target.value);
+  // const handleChange = (e) => setTweets(e.target.value);
 
   return (
-    <Container>
-      <div className="container" style={{ marginTop: 100 }}>
-        <form>
-          <div className="form-group row">
-            {/* <label className="col-sm-3 col-form-label">Search By Username</label> */}
-            <div className="col-sm-9">
-              <input
-                className="form-control"
-                type="text"
-                name="tweet"
-                placeholder="Search Tweets"
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
-            </div>
+    // <Container>
+    <div className="container" style={{ marginTop: 100 }}>
+      <form>
+        <div className="form-group row">
+          {/* <label className="col-sm-3 col-form-label">Search By Username</label> */}
+          <div className="col-sm-9">
+            <input
+              className="form-control"
+              type="text"
+              name="tweet"
+              placeholder="Search Tweets"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
           </div>
+        </div>
 
-          <div className="row">
-            <div className="btn-toolbar">
-              <Button type="submit" className="mr-1" onClick={searchTweets}>
-                Search By Content
-              </Button>
+        <div className="row">
+          <div className="btn-toolbar">
+            <Button
+              type="submit"
+              className="mr-1"
+              onClick={(event) => searchTweets(event)}
+            >
+              Search By Content
+            </Button>
 
-              <Button type="submit" onClick={searchUsers}>
-                Search By User
-              </Button>
-            </div>
+            <Button type="submit" onClick={searchUsers}>
+              Search By User
+            </Button>
           </div>
-          {this.state.tweet.statuses}
-          <div></div>
-        </form>
-      </div>
-    </Container>
+        </div>
+        <div>{setTweets.tweets}</div>
+      </form>
+    </div>
+    // </Container>
   );
 };
 
 export default SearchPage;
-
-{
-  /* <Card style={{ width: "18rem" }}>
-<Card.Body>
-  <Card.Title>Card Title</Card.Title>
-  <Card.Subtitle className="mb-2 text-muted">
-    Card Subtitle
-  </Card.Subtitle>
-  <Card.Text>
-    Some quick example text to build on the card title and make up
-    the bulk of the card's content.
-  </Card.Text>
-  <Card.Link href="#">Card Link</Card.Link>
-  <Card.Link href="#">Another Link</Card.Link>
-</Card.Body>
-</Card> */
-}
