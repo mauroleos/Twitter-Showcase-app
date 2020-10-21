bc49943501833facb5992173428c0eb863fd1cea
mauro Leos <mauroleos94@gmail.com>
2020-10-21 00:05:52 -0500

    Fixed Navbar toggle button

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -2,24 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-light bg-light navbar-expand-lg fixed-top">
-      <h3>
+    <Navbar
+      bg="light"
+      className="navbar navbar-light bg-light navbar-expand-lg fixed-top"
+      expand="lg"
+    >
+      <Navbar.Brand href="#home">
         <FontAwesomeIcon color="#33ccff" size="2x" icon={faTwitter} />
-      </h3>
-      <button
-        className="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#navbarCollapse"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <ul className="navbar-nav ml-auto">
-          <li className="navbar-item"></li>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="navbar-nav ml-auto">
           <Link to="/">
             <li className="navbar-item">Home</li>
           </Link>
@@ -29,9 +26,10 @@ const NavBar = () => {
           <Link to="RandomPage">
             <li className="navbar-item">Random</li>
           </Link>
-        </ul>
-      </div>
-    </nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
+
 export default NavBar;
